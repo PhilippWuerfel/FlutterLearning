4cef05b2f5662b4c49ff7eb4ac2ae80049c545f1
Philipp Würfel <philipp.wuerfel@tu-berlin.de>
2020-02-16 10:28:15 +0100

    adding pluralsight_trip_cost_app to repos

--- pluralsight_app_widgets/lib/ui/home.dart
+++ pluralsight_app_widgets/lib/ui/home.dart
@@ -62,7 +62,7 @@ class Home extends StatelessWidget {
                 ],
               ),
               SurfImageWidget(),
-
+              OrderButton(),
             ])));
   }
 }
@@ -72,7 +72,38 @@ class SurfImageWidget extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
     AssetImage surfAsset = AssetImage('images/surf_icon.png');
-    Image image = Image(image: surfAsset, width: 400, height: 400,);
+    Image image = Image(image: surfAsset, width: 350, height: 350,);
     return Container( child: image);
   }
+}
+
+class OrderButton extends StatelessWidget{
+
+  @override
+  Widget build(BuildContext context) {
+    var button = Container(
+      margin: EdgeInsets.only(top: 50.0),
+      child: RaisedButton(
+        child: Text("Goota catch'em all!"),
+        color: Colors.lightBlue,
+        elevation: 5,
+        onPressed: () {
+          order(context);
+        },
+      )
+    );
+    return button;
+  }
+
+  void order(BuildContext context){
+    var alert = AlertDialog(
+      title: Text("Take off!"),
+      content: Text("Ridin ... ridin ..."),
+    );
+    showDialog(
+        context: context,
+        builder: (BuildContext context)=> alert
+    );
+  }
+
 }
